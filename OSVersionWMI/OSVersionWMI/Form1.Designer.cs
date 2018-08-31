4a975d0ec8f628efb3d9803c9d45abe8ab8716b2
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-08-30 21:59:42 -0400

    OS Version using C# and WMI
    On this tutorial we will show how to use Visual Studio and your C# application to retrieve Windows OS information from your Local machine or remote machines in a domain using WMI (Windows Management Instrumentation)

---
+++ OSVersionWMI/OSVersionWMI/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace OSVersionWMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetOS = new System.Windows.Forms.Button();
+            this.txtOSVersion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetOS
+            // 
+            this.btnGetOS.Location = new System.Drawing.Point(23, 12);
+            this.btnGetOS.Name = "btnGetOS";
+            this.btnGetOS.Size = new System.Drawing.Size(211, 57);
+            this.btnGetOS.TabIndex = 0;
+            this.btnGetOS.Text = "Get OS";
+            this.btnGetOS.UseVisualStyleBackColor = true;
+            this.btnGetOS.Click += new System.EventHandler(this.btnGetOS_Click);
+            // 
+            // txtOSVersion
+            // 
+            this.txtOSVersion.Location = new System.Drawing.Point(23, 75);
+            this.txtOSVersion.Name = "txtOSVersion";
+            this.txtOSVersion.Size = new System.Drawing.Size(211, 20);
+            this.txtOSVersion.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 104);
+            this.Controls.Add(this.txtOSVersion);
+            this.Controls.Add(this.btnGetOS);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetOS;
+        private System.Windows.Forms.TextBox txtOSVersion;
+    }
+}
+
---
+++ OSVersionWMI/OSVersionWMI/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace OSVersionWMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public String Get_OS_Version()
+        {
+
+            String result = "Unable to Retrieve OS Version";
+            ManagementScope scope = null;
+            try
+            {
+                ConnectionOptions options = new ConnectionOptions();
+                options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+                //options.Username = "username";
+                //options.Password = "password";
+                //options.Authority = "NTLMDOMAIN:WORKGROUP";
+
+                scope = new ManagementScope("\\\\" + "PCNAME" + "\\root\\cimv2", options);
+                scope.Connect();
+
+                //Query system for Operating System information
+                ObjectQuery query = new ObjectQuery("SELECT Caption FROM Win32_OperatingSystem");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+                ManagementObjectCollection queryCollection = searcher.Get();
+                foreach (ManagementObject m in queryCollection)
+                {
+                    result = m["Caption"].ToString();// + " " + m["BuildNumber"].ToString(); 
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            scope = null;
+
+            return result;
+
+
+        }
+
+
+
+
+        private void btnGetOS_Click(object sender, EventArgs e)
+        {
+            txtOSVersion.Text = Get_OS_Version();
+        }
+    }
+}
+
